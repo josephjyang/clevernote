f254fd754c60da4cd6cdb56d61e277d27370c5aa
Joe Yang (Joseph) <josephjyang@gmail.com>
2021-12-03 17:10:57 -0500

    error handling middleware

--- authenticate-me/backend/app.js
+++ authenticate-me/backend/app.js
@@ -6,6 +6,7 @@ const helmet = require('helmet');
 const cookieParse = require('cookie-parser');
 const { environment } = require('./config');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 const isProduction = environment === 'production';
 const routes = require('./routes')
 const app = express();
@@ -36,5 +37,35 @@ app.use(csurf({
 }))
 
 app.use(routes);
+
+// Catch unhandled requests and forward to error handler
+app.use((req, res, next) => {
+    const err = new Error("The requested resources couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+})
+
+// Process sequelize errors
+app.use((err, req, res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map(e => e.message);
+        err.title = 'Validation error';
+    }
+    next(err);
+})
+
+// Error formatter
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || "Server Error",
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 module.exports = app;
