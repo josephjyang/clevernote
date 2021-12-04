9614cc53548fb65d0b9a1b2c2c1cef94807c5978
Joe Yang (Joseph) <josephjyang@gmail.com>
2021-12-04 10:05:26 -0500

    dropdown nav menu

--- authenticate-me/frontend/src/components/Navigation/ProfileButton.js
+++ authenticate-me/frontend/src/components/Navigation/ProfileButton.js
@@ -1,9 +1,53 @@
+import { set } from "js-cookie";
 import React, { useState, useEffect } from "react";
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
 
 function ProfileButton({user}) {
-    return <i class="fas fa-user-circle"></i>
+    const dispatch = useDispatch();
+    const [showMenu, setShowMenu] = useState(false)
+
+    const handleClick = () => {
+        setShowMenu(true);
+    }
+
+    const logout = (e) => {
+        e.preventDefault();
+        dispatch(sessionActions.logout());
+    };
+
+    const openMenu = () => {
+        if (showMenu) return;
+        return setShowMenu(true)
+    }
+
+    useEffect(() => {
+        if (!showMenu) return;
+        const closeMenu = () => {
+            setShowMenu(false);
+        }
+
+        document.addEventListener("click", closeMenu)
+
+        return () => document.removeEventListener("click", closeMenu)
+    }, [showMenu])
+
+    return (
+        <>
+            <button onClick={handleClick}>
+                <i class="fas fa-user-circle"></i>
+            </button>
+            {showMenu && (
+                <ul className="profile-dropdown">
+                    <li>{user.username}</li>
+                    <li>{user.email}</li>
+                    <li>
+                        <button onClick={logout}>Log Out</button>
+                    </li>
+                </ul>
+            )}
+        </>
+    )
 }
 
 export default ProfileButton
