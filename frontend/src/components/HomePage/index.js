3d68006654ddb1dc73bfca9d6c1165376ffe130f
Joe Yang <josephjyang@gmail.com>
2022-03-08 18:06:51 -0800

    splash page updates

--- frontend/src/components/HomePage/index.js
+++ frontend/src/components/HomePage/index.js
@@ -4,6 +4,7 @@ import { NavLink, Redirect } from 'react-router-dom';
 import { Modal } from "../../context/Modal"
 import LoginForm from '../LoginFormModal/LoginForm';
 import Navigation from '../Navigation';
+import splash_clevernote from './splash_clevernote.svg'
 import './HomePage.css'
 
 function HomePage({ isLoaded }) {
@@ -17,30 +18,35 @@ function HomePage({ isLoaded }) {
     if (!sessionUser) return (
         <>
             <div id="container">
-                <Navigation isLoaded={isLoaded} />
+                {/* <Navigation isLoaded={isLoaded} /> */}
                 <div id="homepage">
-                    <h1>
-                        Save your ideas, shock the world
-                    </h1>
-                    <h3>
-                        Write down all of your brilliant ideas so that you don't miss out on sharing them with the world.
-                    <br />
-                        Organize your business plans, jokes, solutions, and more, all in one place.
-                    </h3>
-                    <NavLink to="/signup">
+                    <div id="splash-img-ctr">
+                        <img id="splash-img" src={splash_clevernote} alt="splash" />
+                    </div>
+                    <div id="home-content-ctr">
+                        <h1>
+                            Save your ideas, shock the world
+                        </h1>
+                        <h3>
+                            Write down all of your brilliant ideas so that you don't miss out on sharing them with the world.
+                        <br />
+                            Organize your business plans, jokes, solutions, and more, all in one place.
+                        </h3>
+                        <NavLink to="/signup">
 
-                        <button id="sign-up-btn">
-                            Sign up for free
-                        </button>
-                    </NavLink>
-                    <p id="log-in-text">
-                        <button id="log-in-link" onClick={() => setShowModal(true)}>Already have an account? Log in</button>
-                        {showModal && (
-                            <Modal onClose={() => setShowModal(false)}>
-                                <LoginForm />
-                            </Modal>
-                        )}
-                    </p>
+                            <button id="sign-up-btn">
+                                Sign up for free
+                            </button>
+                        </NavLink>
+                        <p id="log-in-text">
+                            <button id="log-in-link" onClick={() => setShowModal(true)}>Already have an account? Log in</button>
+                            {showModal && (
+                                <Modal onClose={() => setShowModal(false)}>
+                                    <LoginForm />
+                                </Modal>
+                            )}
+                        </p>
+                    </div>
                 </div>
             </div>
         </>
